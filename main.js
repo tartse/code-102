bdefdd1dd808823faf262048bcec5691d8d1cdc3
Tariku Kitaw <tartse@yahoo.com>
2018-09-15 14:17:12 -0400

    adding js

--- main.js
+++ main.js
@@ -15,3 +15,23 @@ console.log(city);
 city = "washingtondc";
 console.log(city);
 
+let speed = 50;
+ 
+if (speed<30)
+{
+    console.log("no ticket")
+}
+else if (speed<=40)
+{
+    console.log("50 dollar fine")
+} 
+else {
+    console.log("100 dollar fine")
+}   
+
+let pro = prompt("welcome to my page");
+
+let con;
+con = confirm("Hey buddy,wanna pick some snowberries?")
+
+let al = alert ("you sit on a thorn ")
