34883476a3618814268fc17f5f5447272c8cd5b4
Tariku Kitaw <tartse@yahoo.com>
2018-09-15 11:55:30 -0400

    gh-gages

---
+++ main.js
@@ -0,0 +1,17 @@
+console.log("hello world");
+
+let  box ="bottle";
+console.log(box);
+
+box = "phone";
+console.log(box);
+
+box = "wire";
+console.log(box);
+
+let city = "addisababa"
+console.log(city);
+
+city = "washingtondc";
+console.log(city);
+
